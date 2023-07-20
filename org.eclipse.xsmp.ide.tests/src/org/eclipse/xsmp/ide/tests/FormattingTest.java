36a4edd6567c5204cbd185915c423b9208979609
Cyril Vrousos <135030683+cyrilvrousos-tas@users.noreply.github.com>
2023-07-20 13:24:07 +0200

    22 provide language server protocol (#3)
    * Add VS code extension for XSMP modeler
    
    * Removing qualified version for ls.jar
    
    * Update CI
    
    Signed-off-by: Cyril Vrousos <135030683+cyrilvrousos-tas@users.noreply.github.com>
    
    * Add content assist
    
    * Add hover services on keywords
    
    * Implemented unit tests
    
    ---------
    
    Signed-off-by: Cyril Vrousos <135030683+cyrilvrousos-tas@users.noreply.github.com>
    Co-authored-by: cyrilvrousos-tas <cyril.vrousos@etu.unice.fr>

---
+++ org.eclipse.xsmp.ide.tests/src/org/eclipse/xsmp/ide/tests/FormattingTest.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+* Copyright (C) 2023 THALES ALENIA SPACE FRANCE.
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License 2.0
+* which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+******************************************************************************/
+package org.eclipse.xsmp.ide.tests;
+
+import org.eclipse.xtext.testing.FormattingConfiguration;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class FormattingTest extends AbstractXsmpcatLanguageServerTest
+{
+  @Test
+  public void testFormatting_1()
+  {
+    testFormatting((FormattingConfiguration it) -> {
+      it.setModel(
+              "catalogue TestCatalogue namespace TestNamespace { /** @uuid 87b404c9-2a35-4446-a675-64af8b6a1f68 */ model TestModel { field Bool fea_mybool } }");
+
+      final var expected = """
+
+              catalogue TestCatalogue
+
+
+              namespace TestNamespace
+              {
+                  /** @uuid 87b404c9-2a35-4446-a675-64af8b6a1f68 */
+                  model TestModel
+                  {
+                      field Bool fea_mybool
+                  }
+              }
+
+              """;
+      it.setExpectedText(expected);
+    });
+  }
+
+  @Test
+  @Disabled
+  // TODO remove useless space between description and namespace in Xsmpcat formatting
+  public void testFormatting_2()
+  {
+    testFormatting((FormattingConfiguration it) -> {
+      final var model = """
+              catalogue TestCatalogue
+
+              namespace TestNamespace {
+
+                  /** Description of my event
+
+                  @uuid 4ce8488e-bf6c-4eda-8f08-85e0ac7ce126 */
+                event TestEvent
+              }
+              """;
+      it.setModel(model);
+
+      final var expected = """
+
+              catalogue TestCatalogue
+
+
+              namespace TestNamespace
+              {
+                  /**
+                   * Description of my event
+                   *
+                   * @uuid 4ce8488e-bf6c-4eda-8f08-85e0ac7ce126
+                   */
+                  event TestEvent
+              }
+
+              """;
+      it.setExpectedText(expected);
+    });
+  }
+}
